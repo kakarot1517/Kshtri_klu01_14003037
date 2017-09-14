ee729e8391fdf7d11f7e6f6a65b91adfc2314172
Kumar Bala Naveen SIngh Kshtri <30496213+kakarot1517@users.noreply.github.com>
2017-09-14 15:07:47 +0530

    Add files via upload

---
+++ src/epam_final_temp/Epam_final_temp.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package epam_final_temp;
+
+/**
+ *
+ * @author klu
+ */
+public class Epam_final_temp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+		StudentGroup sg = new StudentGroup(20);
+		sg.getStudents();
+
+    }
+    
+}
